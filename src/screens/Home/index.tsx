bc1272e510ae8c02aa564aa7d20a247dfb0f62df
Anderson Felipe Pereira Vieira <anderson.vieira@a12.com>
2022-09-22 10:42:05 -0300

    primeiro commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Text, TextInput, View, TouchableOpacity  } from "react-native";
+import { styles } from "./styles";
+
+export default function Home(){
+
+  function handleParticipanteAdd(){
+    console.log("Botão clicado");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor='#6B6B6B'
+        />
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleParticipanteAdd}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
